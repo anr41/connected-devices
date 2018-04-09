975fbdb75f8c8c8b98dc5d5aba123ecffc65abe1
anr41 <anr41@case.edu>
2018-04-09 04:34:38 -0400

    Update deploy_lampi.sh

--- Lampi/pkg/deploy_lampi.sh
+++ Lampi/pkg/deploy_lampi.sh
@@ -2,17 +2,18 @@
 
 cd ~/connected-devices/Lampi/pkg/
 
-sudo cp -R ../bluetooth/ lampi/opt/lampi/
-sudo cp -R ../Hardware/ lampi/opt/lampi/
-sudo cp -R ../images/ lampi/opt/lampi/
-sudo cp ../lamp_cmd lampi/opt/lampi/
-sudo cp ../lamp_common.py lampi/opt/lampi/
-sudo cp -R ../lampi/ lampi/opt/lampi/
-sudo cp ../lamp_service.py lampi/opt/lampi/
-sudo cp ../main.py lampi/opt/lampi/lamp_ui.py
-sudo cp -R ../scripts lampi/opt/lampi/
+cp -r ../bluetooth/ lampi/opt/lampi/
+cp -r ../Hardware/ lampi/opt/lampi/
+cp -r ../images/ lampi/opt/lampi/
+cp ../lamp_cmd lampi/opt/lampi/
+cp ../lamp_common.py lampi/opt/lampi/
+cp -r ../lampi/ lampi/opt/lampi/
+cp ../lamp_service.py lampi/opt/lampi/
+cp ../main.py lampi/opt/lampi/lamp_ui.py
+cp -r ../scripts lampi/opt/lampi/
 
-sudo mv lampi/opt/lampi/bluetooth/peripheral.js lampi/opt/lampi/bt_peripheral.js 
+mv lampi/opt/lampi/bluetooth/peripheral.js lampi/opt/lampi/bt_peripheral.js 
+
 bumpversion minor --allow-dirty
 dpkg-deb --build lampi
 reprepro -b ~/connected-devices/Web/reprepro/ubuntu/ includedeb ashley lampi.deb
